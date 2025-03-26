e5908a97fd7c040a70349d5fecadcf4806a70018
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2025-03-26 13:10:50 +0100

    Update App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,27 +1,21 @@
 import { useState, useRef, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link, Navigate, Outlet, useNavigate } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <>
-      <Start_Home />
-    </>
-  )
-}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Start />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/home/singin" element={<SingIn />} />
+          <Route path="/home/singup" element={<SingUp />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
 
-function Start_Home() {
-  const [visibleHome, setVisibleHome] = useState(false);
-  const [visibleStart, setVisibleStart] = useState(true);
-
-  const toHome = () => {
-    setVisibleHome(true);
-  };
-
-  return (
-    <>
-      <Start visible={visibleStart} setVisible={setVisibleStart} switchPage={toHome}/>
-      <Home visible={visibleHome} setVisible={setVisibleHome}/>
+      
     </>
   )
 }
@@ -32,7 +26,7 @@ function Logo({name}) {
   )
 }
 
-function Start({visible, setVisible, switchPage}) {
+function Start() {
   const [animating, setAnimating] = useState(false);
   const [clickPosition, setClickPosition] = useState({ x: 0, y: 0 });
   const menuRef = useRef(null);
@@ -47,57 +41,79 @@ function Start({visible, setVisible, switchPage}) {
       setAnimating(true);
       
       setTimeout(() => {
-        setVisible(false);
-        switchPage();
+        window.location.href = "/home"
       }, 1000);
     }
     
   };
 
   return (
-    visible && (
-      <>
-        <div 
-          ref={menuRef}
-          className={`start_menu ${animating ? 'fading' : ''}`} 
-          onClick={handleClick} >
-          
-          <Logo name="logo_start"/>
+    <>
+      <div 
+        ref={menuRef}
+        className={`start_menu ${animating ? 'fading' : ''}`} 
+        onClick={handleClick} >
+        
+        <Logo name="logo_start"/>
 
-          <div className="content">
-            <h1 className="start_title">WELCOME ON CY-HOUSE !</h1>
-            <h3 className="sub_start">Click to continue.</h3>
-          </div>
-          
-          {animating && (
-            <div 
-              className="shockwave" 
-              style={{
-                left: clickPosition.x,
-                top: clickPosition.y
-              }}
-            ></div>
-          )}
+        <div className="content">
+          <h1 className="start_title">WELCOME ON CY-HOUSE !</h1>
+          <h3 className="sub_start">Click to continue.</h3>
         </div>
-      </>
-    )
+        
+        {animating && (
+          <div 
+            className="shockwave" 
+            style={{
+              left: clickPosition.x,
+              top: clickPosition.y
+            }}
+          ></div>
+        )}
+      </div>
+    </>
   )
 }
 
 
-function Home({visible, setVisible}) {
-  
+function Home() {
   return (
-    visible && (
-      <div className = "home">
-          <h1 className="home_title">HOME PAGE</h1>
-            <Logo name="logo_home"/>
-          <nav>
-            
-          aaaaa
-          </nav>
-      </div>
-    )
+    <div className = "home">
+        <h1 className="home_title">HOME PAGE</h1>
+        <Logo name="logo_home"/>
+        <NavBar1/>
+    </div>
+  )
+}
+
+function SingIn() {
+  return (
+    <div className="singIn">
+
+    </div>
+  )
+}
+
+function SingUp() {
+  return (
+    <div className="singUp">
+
+    </div>
+  )
+}
+
+function NavBar1() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/home/singin">Sing in</Link>
+        </li>
+        <li>
+          <Link to="/home/singup">Sing up</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
