f9d7253313dfef3c334c763dd11e0768379427c6
Damien.M <133344364+MouchardDamien@users.noreply.github.com>
2025-04-01 19:15:59 +0200

    Delete src directory

--- src/components/logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Logo({className}) {
-    return (
-      <img className={className} src="/src/assets/logoCYhouse.png" alt="Logo de CY-House."/>
-    )
-  }
-
-  export default Logo
